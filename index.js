281d03400d87a8b8fa0360be3ba9e4f1ed2cb200
Dhohir Pradana <dhohirkudus@gmail.com>
2023-10-30 11:28:50 +0700

    fix: update

--- index.js
+++ index.js
@@ -327,28 +327,140 @@ app.put('/job/:id', (req, res) => {
 
     const { type, data, schedule } = req.body;
 
-    if (type === undefined || data === undefined || schedule === undefined) {
-        return res.status(400).json({ message: 'type, data, and schedule are required' });
+    if (!type || !data || !schedule) {
+        return res.status(400).json({ message: 'type, data and schedule are required' });
     }
 
-    const updatedJob = {
-        id,
-        type,
-        data,
-        schedule,
-        updated_at: formattedDatetime,
-    };
+    var cronTime = schedule;
 
-    const index = jobs.findIndex((job) => job.id === id);
+    if (!cronTime || !cron.validate(cronTime)) {
+        return res.status(400).json({ message: 'cronTime is required' });
+    }
 
-    if (index !== -1) {
-        jobs[index] = updatedJob;
-        const jobsString = JSON.stringify(jobs);
-        res.json({ success: true, message: `Job ${id} updated`, updatedJob, jobs, jobsString });
+    if (!listType.includes(type)) {
+        return res.status(400).json({ message: 'type is not valid' });
+    }
+
+    const job = jobs.find((job) => job.id === id);
+    if (job) {
+        // stop job
+        job.task.stop();
+
+        const index = jobs.indexOf(job);
+        jobs.splice(index, 1);
+
+        var jobStatus = "running";
+
+        // get jobsString data
+        const jobString = jobsString.find((jobString) => jobString.id === id);
+        if (jobString) {
+            jobStatus = jobString.status;
+            const index = jobsString.indexOf(jobString);
+            jobsString.splice(index, 1);
+        }
+
+        if (type == "httpRequest") {
+            // parse data body
+            var httpData = data;
+
+            // validate data must object and have url, method, headers, body
+            if (!httpData || !httpData.url || !httpData.method || !httpData.headers || !httpData.body) {
+                return res.status(400).json({ message: 'data is not valid, must object have url, method, headers, body' });
+            }
+
+            if (!listMethod.includes(httpData.method.toUpperCase())) {
+                return res.status(400).json({ message: 'method is not valid' });
+            }
+
+            // validate headers must object
+            if (typeof httpData.headers !== 'object') {
+                return res.status(400).json({ message: 'headers is not valid, must object' });
+            }
+
+            // validate body must object
+            if (typeof httpData.body !== 'object') {
+                return res.status(400).json({ message: 'body is not valid, must object' });
+            }
+
+            // create http request every method
+            var url = httpData.url;
+
+            // validate url format must start with http:// or https://
+            if (!url.startsWith("https://") && !url.startsWith("http://")) {
+                return res.status(400).json({ message: 'url is not valid' });
+            }
+
+            // // validate url format must http or https
+            // var urlFormatHttps = /^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+            // var urlFormatHttp = /^http?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/;
+
+            // if (!urlFormatHttps.test(url) && !urlFormatHttp.test(url)) {
+            //     return res.status(400).json({ message: 'url is not valid' });
+            // }
+
+            var method = httpData.method;
+            var headers = httpData.headers;
+            var body = httpData.body;
+
+            const task = cron.schedule(cronTime, async () => {
+                try {
+                    const response = await axios({
+                        method: method,
+                        url: url,
+                        headers: headers,
+                        data: body,
+                        httpsAgent: httpsAgent
+                    });
+                    console.log(response.data);
+                } catch (error) {
+                    console.error(error);
+                }
+            });
+
+            // stop task if job status is not running
+            if (jobStatus != "running") {
+                task.stop();
+            }
+
+            jobs.push({ id: uuid, task });
+
+            jobsString.push({ id: uuid, type, data, schedule, created_at: formattedDatetime, status: jobStatus });
+            res.status(201).json({ message: "Task updated!", data: { id: uuid, type, data, schedule, created_at: formattedDatetime, status: jobStatus } });
+        } else if (type == "command") {
+            // validate data must string
+            if (typeof data !== 'string') {
+                return res.status(400).json({ message: 'data is not valid, must string' });
+            }
+
+            // create command
+            var command = data;
+
+            const task = cron.schedule(cronTime, async () => {
+                try {
+                    exec(command, (error, stdout, stderr) => {
+                        if (error) {
+                            console.log(`error: ${error.message}`);
+                            return;
+                        }
+                        if (stderr) {
+                            console.log(`stderr: ${stderr}`);
+                            return;
+                        }
+                        console.log(`stdout: ${stdout}`);
+                    });
+                } catch (error) {
+                    console.error(error);
+                }
+            });
+
+            jobs.push({ id: uuid, task });
+            jobsString.push({ id: uuid, type, data, schedule, created_at: formattedDatetime, status: "running" });
+            res.status(201).json({ message: "Task updated!", data: { id: uuid, type, data, schedule, created_at: formattedDatetime, status: "running" } });
+        }
+
     } else {
         res.status(404).json({ message: 'Job not found' });
     }
-    saveJobsToFile();
 });
 
 // stop job
